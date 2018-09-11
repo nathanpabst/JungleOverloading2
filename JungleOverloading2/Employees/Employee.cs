f5eb723ccf6718ec7755087adf32e9ac91ab648e
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-10 21:19:29 -0500

    copied files into new project

---
+++ JungleOverloading2/Employees/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JungleOverloading2.Employees
+{
+    class Employee
+    {
+        //properties
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private List<string> Restaurants { get; } = new List<string> { "Texas Tom's", "Pizza Palace", "Midieval Times", "The Dumpster" };
+        private Random rando = new Random();
+
+        //Constructor with two parameters
+        public Employee(string first, string last)
+        {
+            FirstName = first;
+            LastName = last;
+
+        }
+        //Methods
+        public void Eat()
+        {
+            var restaurant = Restaurants[rando.Next(0, Restaurants.Count)];
+            Console.WriteLine($"{FirstName} is eating at {restaurant}");
+        }
+
+        public void Eat(string food)
+        {
+            Console.WriteLine($"{FirstName} ate {food} in the office.");
+        }
+
+        public void Eat(List<Employee> pals)
+        {
+            var resturant = Restaurants[rando.Next(0, Restaurants.Count)];
+            var palList = new List<string>();
+            foreach (var pal in palList)
+            {
+                palList.Add(pal.FirstName);
+            }
+            var palString = string.Join(",", palList);
+
+            Console.WriteLine($"{FirstName} ate at {resturant} with {palString}");
+        }
+
+    }
+}
--- JungleOverloading2/Program.cs
+++ JungleOverloading2/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using JungleOverloading2.Employees;
+
 
 namespace JungleOverloading2
 {
@@ -6,7 +9,18 @@ namespace JungleOverloading2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //instantiating Employee class
+            var palOne = new Employee("Bob", "Lahblah");
+            var palTwo = new Employee("Rachel", "Hunter");
+            var palThree = new Employee("Frank", "Ley");
+            var palFour = new Employee("Nicole", "Jones");
+
+            var palOnesPals = new List<Employee> { palTwo, palThree, palFour };
+
+            palOne.Eat();
+            palTwo.Eat("fish");
+            palOne.Eat(palOnesPals);
+            Console.ReadLine();
         }
     }
 }
